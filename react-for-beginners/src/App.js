d366e605d2c24017b91cb087ec6cdb9b95d9a614
Chang-Young Park <parkcy0630@gmail.com>
2024-04-01 22:57:18 +0900

    #7.2 Coin Tracker
    240401

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,41 +1,31 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  // toDo, toDos나 직접적으로 수정 불가능 >>> 함수를 가져와서 수정하게 만들어야함(setToDo, setToDos)
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault(); // 새로 고침 방지
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      {/* form은 submit 이벤트를 갖고 있음 */}
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {/* map((item, index) => {item}) >>> argument = 값 // index  = 숫자 */}
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}: ${coin.quotes.USD.price})
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
