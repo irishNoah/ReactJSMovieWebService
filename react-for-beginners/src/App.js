1fb57cbb607425bc1fe50459b5726e1b7f8133ab
Chang-Young Park <parkcy0630@gmail.com>
2024-04-01 21:42:40 +0900

    #7.0 To Do List part One
    240401

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,23 +1,34 @@
-import userEvent from "@testing-library/user-event";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  /* cleanup -> 니꼬쌤 왈 : cleanup을 React 개발 시 자주 사용 X */
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  // toDo, toDos나 직접적으로 수정 불가능 >>> 함수를 가져와서 수정하게 만들어야함(setToDo, setToDos)
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); // 새로 고침 방지
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      {/* form은 submit 이벤트를 갖고 있음 */}
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
