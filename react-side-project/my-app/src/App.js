80114e0059b68f7737b3146f3340690016b09056
Chang-Young Park <parkcy0630@gmail.com>
2024-04-17 23:45:58 +0900

    20240417(0) - useState

--- react-side-project/my-app/src/App.js
+++ react-side-project/my-app/src/App.js
@@ -1,16 +1,14 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render!!!");
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!!</h1>
-
-      {/* 
-          - text에 값을 아무것도 안넣을 경우, 에러가 발생함
-            - Button.js에서 propTypes을 필수(isRequired)로 설정했기 때문이다.
-      */}
-      <Button text={"Touch Button"} />
+      <h1>Click Count is {counter}</h1>
+      <button onClick={onClick}>Click this Button</button>
     </div>
   );
 }
