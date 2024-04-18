eec4e2988ac7c7ba5ad693b53c0965b81d95879a
Chang-Young Park <parkcy0630@gmail.com>
2024-04-18 21:58:00 +0900

    20240418(1) - useEffect Basic

--- react-side-project/my-app/src/App.js
+++ react-side-project/my-app/src/App.js
@@ -1,31 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((cnt) => cnt + 1);
-
-  const [inputVal, setInputVal] = useState("");
-  const onHandleChange = (e) => setInputVal(e.target.value);
-
-  const [liked, setLiked] = useState(true);
-  function onHandleCheck(e) {
-    setLiked(e.target.checked);
-  }
-  //const onHandleCheck = (e) => setLiked(e.target.checked);
+  const [number, setNumber] = useState(0);
+  const onHandleClick = () => {
+    setNumber((bef) => bef + 1);
+  };
+  console.log("Click Button Render!");
+  useEffect(() => console.log("Render First Time Only..."), []);
 
   return (
     <div>
-      <button onClick={onClick}>Click number is {counter}!!!</button>
-      <br />
-      <br />
-      <input value={inputVal} onChange={onHandleChange} />
-      <p>Text is {inputVal}</p>
-
-      <label>
-        <input type="checkbox" checked={liked} onChange={onHandleCheck} />I
-        liked this!
-      </label>
-      <p>You {liked ? "liked" : "did not like"} programming!</p>
+      <h1>This is irishNoah Web Page!</h1>
+      <h1>{number}</h1>
+      <button onClick={onHandleClick}>Click Here!</button>
     </div>
   );
 }
