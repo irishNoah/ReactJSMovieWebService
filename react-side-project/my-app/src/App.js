670ae3c392c9577830e061d8d0ecef0fed77cf67
Chang-Young Park <parkcy0630@gmail.com>
2024-04-24 22:22:35 +0900

    20240424(0) API(Coin Tracker)

--- react-side-project/my-app/src/App.js
+++ react-side-project/my-app/src/App.js
@@ -1,41 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState("");
+  const [selectedCoin, setSelectedCoin] = useState(null); // 선택한 코인을 보유하고 있는 상태
 
-  const onChange = (e) => {
-    setToDo(e.target.value);
-    console.log(e);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  // 드롭다운에서 새 코인을 선택할 때 처리하는 기능
+  const handleSelectChange = (event) => {
+    const coinId = event.target.value; // 선택 옵션 값에서 코인 ID를 가져오기
+    const coin = coins.find((coin) => coin.id === coinId); // ID를 기준으로 선택한 코인을 찾기
+    setSelectedCoin(coin); // 선택한 코인 상태 설정
   };
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((nowToDoArray) => [toDo, ...nowToDoArray]);
-    setToDo("");
-  };
-  console.log(toDos);
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={toDo}
-          onChange={onChange}
-          placeholder="Input Your TO Do List"
-        />
-        <button>Add To Do</button>
-      </form>
-
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The number of coins is {loading ? 0 : `${coins.length}`}</h1>
+      {loading ? (
+        <h2>Coins Page is now loading...</h2>
+      ) : (
+        <div>
+          <label>Choose Coin!</label>
+          <br />
+          <select onChange={handleSelectChange}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.id}>
+                {coin.name}
+              </option> // coin.id를 키와 값으로 사용
+            ))}
+          </select>
+          {selectedCoin && ( // 선택한 코인이 있는지 확인하고 해당 코인의 세부정보를 표시
+            <p>
+              Selected Coin: {selectedCoin.name} <br />
+              Symbol:{selectedCoin.symbol} <br />
+              total_supply : {selectedCoin.total_supply} <br />
+              first_data_at : {selectedCoin.first_data_at} <br />
+              USD price : {selectedCoin.quotes.USD.price}
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
