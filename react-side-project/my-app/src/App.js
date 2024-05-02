36172a5729f4368306ba0cc06bf202bf6ac1824d
Chang-Young Park <parkcy0630@gmail.com>
2024-05-02 23:12:52 +0900

    20240502 Movie API & React-Router-Dom

--- react-side-project/my-app/src/App.js
+++ react-side-project/my-app/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
   return (
-    <div>
-      <h1>Now, Remodeling IrishNoah Cinema...</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/test">
+          <h1>Test Page</h1>
+        </Route>
+        <Route path="/bye">
+          <h1>bye Page</h1>
+        </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
